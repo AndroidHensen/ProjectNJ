615f9fedd748f88af2c3f07cea52e2fb47353ee0
Try118 <1021996158@qq.com>
2018-01-13 21:16:09 +0800

    提交人：姚喜 提交内容：在“工作台”模块添加了【工作任务】下的【项目任务】下的“反馈任务”页面

---
+++ app/src/main/java/com/handsome/projectnz/V/Home/project_feedback_task.java
@@ -0,0 +1,38 @@
+package com.handsome.projectnz.V.Home;
+
+import android.view.View;
+
+import com.handsome.module_engine.E.BaseTemplate.BaseActivity;
+import com.handsome.projectnz.R;
+
+/**
+ * Created by YX_PC on 2018/1/13.
+ */
+
+public class project_feedback_task extends BaseActivity {
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_project_feedback_task;
+    }
+
+    @Override
+    public void initViews() {
+
+    }
+
+    @Override
+    public void initListener() {
+
+    }
+
+    @Override
+    public void initData() {
+        setTitle("反馈任务");
+        setTitleCanBack();
+    }
+
+    @Override
+    public void processClick(View v) {
+
+    }
+}
