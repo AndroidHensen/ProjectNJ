02602c3e0b61ef659bc1c1bd64d7c29372ec621c
Diko <1198611857@qq.com>
2018-01-13 19:45:55 +0800

    提交内容: 1.修改Mine（我的）模块的主界面。

--- app/src/main/java/com/handsome/projectnz/F/MineFragment.java
+++ app/src/main/java/com/handsome/projectnz/F/MineFragment.java
@@ -4,12 +4,17 @@ import android.view.View;
 
 import com.handsome.module_engine.E.BaseTemplate.BaseLazyFragment;
 import com.handsome.projectnz.R;
+import com.handsome.projectnz.V.MyView.FastMenuBar;
 
 /**
- * Created by handsome on 2016/4/7.
+ * Created by Diko on 2018/1/13.
  */
 public class MineFragment extends BaseLazyFragment {
-
+    private FastMenuBar fmb_company_information;
+    private FastMenuBar fmb_help;
+    private FastMenuBar fmb_cache_clean_up;
+    private FastMenuBar fmb_feedback;
+    private FastMenuBar fmb_setting;
     @Override
     public int getLayoutId() {
         return R.layout.fragment_mine;
@@ -17,9 +22,12 @@ public class MineFragment extends BaseLazyFragment {
 
     @Override
     public void initViews() {
-
+        fmb_company_information=findView(R.id.company_information);
+        fmb_help=findView(R.id.help);
+        fmb_cache_clean_up=findView(R.id.cache_clean_up);
+        fmb_setting=findView(R.id.setting);
+        fmb_feedback=findView(R.id.feedback);
     }
-
     @Override
     public void initListener() {
 
