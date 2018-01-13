a40978771b8b8d8f7a4ad433dc606725effe59df
ljtlcj <1521826348@qq.com>
2018-01-13 21:11:31 +0800

    提交人：林楚杰 提交内容：添加登录模块中的“单位注册”界面

---
+++ app/src/main/java/com/handsome/projectnz/V/Home/Login_register_untiActivity.java
@@ -0,0 +1,38 @@
+package com.handsome.projectnz.V.Home;
+
+import android.view.View;
+
+import com.handsome.module_engine.E.BaseTemplate.BaseActivity;
+import com.handsome.projectnz.R;
+
+/**
+ * Created by jie on 2018/1/13.
+ */
+
+public class Login_register_untiActivity extends BaseActivity {
+    @Override
+    public int getLayoutId() {
+        return R.layout.login_register_registerunit;
+    }
+
+    @Override
+    public void initViews() {
+
+    }
+
+    @Override
+    public void initListener() {
+
+    }
+
+    @Override
+    public void initData() {
+        setTitle("注册");
+        setTitleCanBack();
+    }
+
+    @Override
+    public void processClick(View v) {
+
+    }
+}
