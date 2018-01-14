6a502a9a91b2997865d588f09eff0080bb6e2631
Try118 <1021996158@qq.com>
2018-01-14 19:57:51 +0800

    提交人：姚喜 提交内容：二次修改“工作台”模块【工作任务】下的【项目任务】下的“反馈任务”页面

--- app/src/main/java/com/handsome/projectnz/View/Home/ReturnProjectTaskActivity.java
+++ app/src/main/java/com/handsome/projectnz/View/Home/ReturnProjectTaskActivity.java
@@ -1,6 +1,8 @@
 package com.handsome.projectnz.View.Home;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.handsome.module_engine.E.BaseTemplate.BaseActivity;
 import com.handsome.projectnz.R;
@@ -10,8 +12,11 @@ import com.handsome.projectnz.R;
  */
 
 public class ReturnProjectTaskActivity extends BaseActivity {
-
-
+    private EditText et_project_name;
+    private EditText et_task_name;
+    private EditText ed_input;
+    private EditText et_explain;
+    private Button btn_submit;
     @Override
     public int getLayoutId() {
         return R.layout.activity_return_project_task;
@@ -19,7 +24,11 @@ public class ReturnProjectTaskActivity extends BaseActivity {
 
     @Override
     public void initViews() {
-
+        et_project_name = findView(R.id.et_project_name);
+        et_task_name=findView(R.id.et_task_name);
+        ed_input=findView(R.id.ed_input);
+        et_explain=findView(R.id.et_explain);
+        btn_submit=findView(R.id.btn_submit);
     }
 
     @Override
