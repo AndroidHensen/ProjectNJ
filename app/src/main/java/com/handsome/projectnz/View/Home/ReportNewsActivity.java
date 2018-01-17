55b93ff150c27f3ec8e09ff921537fe7e6777382
Try118 <1021996158@qq.com>
2018-01-17 23:00:16 +0800

    提交人：姚喜 提交内容：增加“工作台”模块【新建汇报】界面

---
+++ app/src/main/java/com/handsome/projectnz/View/Home/ReportNewsActivity.java
@@ -0,0 +1,49 @@
+package com.handsome.projectnz.View.Home;
+
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.handsome.module_engine.E.BaseTemplate.BaseActivity;
+import com.handsome.projectnz.R;
+
+/**
+ * Created by jie on 2018/1/15.
+ */
+
+public class ReportNewsActivity extends BaseActivity {
+    private EditText et_report_title;
+    private EditText et_report_content;
+    private ImageButton ib_camera;
+    private ImageButton ib_right;
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_announcement_new;
+    }
+
+    @Override
+    public void initViews() {
+        et_report_title = (EditText) findViewById(R.id.et_report_title);
+        et_report_content = (EditText) findViewById(R.id.et_report_content);
+        ib_camera = (ImageButton) findViewById(R.id.ib_camera);
+        ib_right = (ImageButton) findViewById(R.id.ib_right);
+    }
+
+    @Override
+    public void initListener() {
+
+    }
+
+    @Override
+    public void initData() {
+        setEdit2("提交");
+        setTitleCanBack();
+        setTitle("新建汇报");
+    }
+
+    @Override
+    public void processClick(View v) {
+
+    }
+}
