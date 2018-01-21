ec0f9a2842b40387cec855dc774bb892094f7dc5
ljtlcj <1521826348@qq.com>
2018-01-22 04:02:29 +0800

    提交人：林楚杰 提交内容：修改代码

--- app/src/main/java/com/handsome/projectnz/Fragment/HomeFragment.java
+++ app/src/main/java/com/handsome/projectnz/Fragment/HomeFragment.java
@@ -2,6 +2,7 @@ package com.handsome.projectnz.Fragment;
 
 import android.content.Intent;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.handsome.module_engine.E.BaseTemplate.BaseLazyFragment;
 import com.handsome.projectnz.R;
@@ -12,7 +13,21 @@ import com.handsome.projectnz.View.Home.RegisterActivity;
  * Created by handsome on 2016/4/7.
  */
 public class HomeFragment extends BaseLazyFragment {
-
+    private ImageView iv_company_announcements;//公司公告
+    private ImageView iv_working_task;//工作任务
+    private ImageView iv_work_report;//工作汇报
+    private ImageView iv_general_settings;//通用申请
+    private ImageView iv_detailed_list;//清单
+    private ImageView iv_progress_plan;//进度计划
+    private ImageView tv_security_check;//安全检查
+    private ImageView iv_quality_inspection;//质量检查
+    private ImageView iv_record_workpoints;//签到记工
+    private ImageView iv_completion_test;//完工验收
+    private ImageView iv_materials;//物资
+    private ImageView iv_statistics_task;//任务统计
+    private ImageView iv_hard_statistics;//用工统计
+    private ImageView iv_material_statistics;//物资统计
+    private ImageView iv_data_statistics;//资料统计
     @Override
     public int getLayoutId() {
         return R.layout.fragment_home;
@@ -20,7 +35,21 @@ public class HomeFragment extends BaseLazyFragment {
 
     @Override
     public void initViews() {
-
+        iv_company_announcements = findView(R.id.iv_company_announcements);
+        iv_working_task = findView(R.id.iv_working_task);
+        iv_work_report = findView(R.id.iv_work_report);
+        iv_general_settings = findView(R.id.iv_general_settings);
+        iv_detailed_list = findView(R.id.iv_detailed_list);
+        iv_progress_plan = findView(R.id.iv_progress_plan);
+        tv_security_check = findView(R.id.tv_security_check);
+        iv_record_workpoints = findView(R.id.iv_record_workpoints);
+        iv_quality_inspection = findView(R.id.iv_quality_inspection);
+        iv_completion_test = findView(R.id.iv_completion_test);
+        iv_materials = findView(R.id.iv_materials);
+        iv_statistics_task = findView(R.id.iv_statistics_task);
+        iv_hard_statistics = findView(R.id.iv_hard_statistics);
+        iv_material_statistics = findView(R.id.iv_material_statistics);
+        iv_data_statistics = findView(R.id.iv_data_statistics);
     }
 
     @Override
