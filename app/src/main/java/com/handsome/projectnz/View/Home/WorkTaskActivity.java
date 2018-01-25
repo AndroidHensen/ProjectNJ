1aad0f72915439693d37c2711e6669a4ef23eb2d
Try118 <1021996158@qq.com>
2018-01-25 20:08:33 +0800

    提交人：姚喜 提交内容：1.添加_工作台_通用申请页面   2.添加_工作台_通用申请_申请详情_页面

--- app/src/main/java/com/handsome/projectnz/View/Home/WorkTaskActivity.java
+++ app/src/main/java/com/handsome/projectnz/View/Home/WorkTaskActivity.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.handsome.module_engine.E.BaseTemplate.BaseActivity;
 import com.handsome.projectnz.Adapter.MainAdapter;
@@ -25,6 +26,8 @@ public class WorkTaskActivity extends BaseActivity implements ViewPager.OnPageCh
     private ViewPager vp_work_task;
     private ArrayList<Fragment> list;
     private MainAdapter adapter;
+    private ImageButton ib_find;
+    private ImageButton ib_add;
 
     @Override
     public int getLayoutId() {
@@ -33,6 +36,8 @@ public class WorkTaskActivity extends BaseActivity implements ViewPager.OnPageCh
 
     @Override
     public void initViews() {
+        ib_find = findView(R.id.ib_find);
+        ib_add = findView(R.id.ib_add);
         btn_assign = findView(R.id.btn_assign);
         btn_arrange = findView(R.id.btn_arrange);
         vp_work_task = findView(R.id.vp_work_task);
@@ -49,16 +54,17 @@ public class WorkTaskActivity extends BaseActivity implements ViewPager.OnPageCh
     public void initData() {
         setTitle("工作任务");
         setTitleCanBack();
-       initFragments();
+        initFragments();
     }
+
     /**
      * 初始化碎片
      */
     private void initFragments() {
-        list=new ArrayList<Fragment>();
+        list = new ArrayList<Fragment>();
         list.add(new WorkTaskAssignFragment());
         list.add(new WorkTaskArrangeFragment());
-        adapter=new MainAdapter(this.getSupportFragmentManager(),list);
+        adapter = new MainAdapter(this.getSupportFragmentManager(), list);
         vp_work_task.setAdapter(adapter);
         vp_work_task.setOffscreenPageLimit(1);
         onPageSelected(0);
@@ -66,7 +72,7 @@ public class WorkTaskActivity extends BaseActivity implements ViewPager.OnPageCh
 
     @Override
     public void processClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.btn_assign:
                 onPageSelected(0);
                 break;
@@ -84,12 +90,12 @@ public class WorkTaskActivity extends BaseActivity implements ViewPager.OnPageCh
     @Override
     public void onPageSelected(int position) {
         vp_work_task.setCurrentItem(position);
-        if(position==0){
+        if (position == 0) {
             btn_assign.setBackgroundColor(Color.WHITE);
             btn_assign.setTextColor(Color.parseColor("#3F51B5"));
             btn_arrange.setBackgroundColor(Color.parseColor("#3F51B5"));
             btn_arrange.setTextColor(Color.WHITE);
-        }else{
+        } else {
             btn_assign.setBackgroundColor(Color.parseColor("#3F51B5"));
             btn_assign.setTextColor(Color.WHITE);
             btn_arrange.setBackgroundColor(Color.WHITE);
