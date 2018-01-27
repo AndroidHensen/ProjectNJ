da63af2d1fab16cf8d2592bc3e394c23448f91af
AndroidHensen <510402535@qq.com>
2018-01-27 12:23:44 +0800

    提交人：许英俊 提交内容：增加后台接口

--- app/src/main/java/com/handsome/projectnz/Utils/RetrofitUtils.java
+++ app/src/main/java/com/handsome/projectnz/Utils/RetrofitUtils.java
@@ -3,12 +3,19 @@ package com.handsome.projectnz.Utils;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+import retrofit2.http.PartMap;
 
 /**
  * =====作者=====
@@ -18,7 +25,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 public class RetrofitUtils {
 
-    private static final String ObjectUrl = "http://106.14.175.111/index.php/";
+    private static final String ObjectUrl = "http: //106.14.175.111/index.php/api/";
     private static Retrofit retrofit = null;
     private static IRetrofitServer iServer;
 
@@ -53,8 +60,50 @@ public class RetrofitUtils {
         return requestBody;
     }
 
-
     public interface IRetrofitServer {
+        //显示所有项目物资
+        @Multipart
+        @POST("goods/showAll")
+        Call<ResponseBody> showAll(@PartMap Map<String, RequestBody> map, @Part List<MultipartBody.Part> parts);
 
+        //新建入库
+        @Multipart
+        @POST("goods/createGoods")
+        Call<ResponseBody> createGoods(@PartMap Map<String, RequestBody> map, @Part List<MultipartBody.Part> parts);
+
+        //新建入库1
+        @Multipart
+        @POST("goods/addGoods")
+        Call<ResponseBody> addGoods(@PartMap Map<String, RequestBody> map, @Part List<MultipartBody.Part> parts);
+
+        //新建出库
+        @Multipart
+        @POST("goods/deleteGoods")
+        Call<ResponseBody> deleteGoods(@PartMap Map<String, RequestBody> map, @Part List<MultipartBody.Part> parts);
+
+        //显示物资流水
+        @Multipart
+        @POST("goods/showAll")
+        Call<ResponseBody> showList(@PartMap Map<String, RequestBody> map, @Part List<MultipartBody.Part> parts);
+
+        //获取入库记录
+        @Multipart
+        @POST("goods/recordIn")
+        Call<ResponseBody> recordIn(@PartMap Map<String, RequestBody> map, @Part List<MultipartBody.Part> parts);
+
+        //获取出库记录
+        @Multipart
+        @POST("goods/recordOut")
+        Call<ResponseBody> recordOut(@PartMap Map<String, RequestBody> map, @Part List<MultipartBody.Part> parts);
+
+        //将单位注册页面的数据存进数据表
+        @Multipart
+        @POST("register/unit_register")
+        Call<ResponseBody> unit_register(@PartMap Map<String, RequestBody> map, @Part List<MultipartBody.Part> parts);
+
+        //将个人注册页面的开户许可存进数据表
+        @Multipart
+        @POST("register/personal_register")
+        Call<ResponseBody> personal_register(@PartMap Map<String, RequestBody> map, @Part List<MultipartBody.Part> parts);
     }
 }
